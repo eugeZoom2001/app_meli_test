df3421c5a7f8d4e52843b1b39b5ddf9074914ad0
euge <eugenionoche2@gmail.com>
2024-11-17 16:06:56 -0300

    socket client config U

--- public/js/constantes.mjs
+++ public/js/constantes.mjs
@@ -1,6 +1,6 @@
 // sockets
 export const socketConfig = {
-  url: "http://localhost:8800",
+  url: "https://app-meli-test.onrender.com",
   //url: "https://rm21thw.localto.net",
   config: { transports: ["websocket", "polling", "flashsocket"] },
 };
